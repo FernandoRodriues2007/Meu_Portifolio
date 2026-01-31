7278bbf51bbd23515e0501c6fb43032c6ee1d755
FernandoRodriues007 <fernandomateusrodriguesfernand@gmail.com>
2026-01-31 09:24:13 +0100

    atualizando...

---
+++ assets/js/validacao.js
@@ -0,0 +1,6 @@
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Obrigado por entrar em contato! Responderemos em breve.');
+    this.reset();
+});
